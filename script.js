e0cef0981e32c8f1718c43ce823a109c8f328a02
yashsharma00148 <166123150+yashsharma00148@users.noreply.github.com>
2024-05-15 15:39:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ let hamburger=document.querySelector(".hamburger")
 let nav_list=document.querySelector(".nav-list")
 hamburger.addEventListener("click",()=>{
     if(hamburger.classList.contains("fa-bars")){
-        nav_list.style.right="0px"
+        nav_list.style.display="flex"
         hamburger.classList.remove("fa-bars")
         hamburger.classList.add("fa-x")}
 
 
         else{
-            nav_list.style.right="-300px"
+            nav_list.style.display="none"
             hamburger.classList.remove("fa-x")
             hamburger.classList.add("fa-bars")
         }
